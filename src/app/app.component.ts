9d76b1511c18976c0e093312b556165922940a31
akash mane <akashmane@akashs-MacBook-Air.local>
2020-07-21 22:26:52 +0530

    local storage added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,68 +1,56 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 import { Calender } from './calender.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'calender-app';
-  timeslot =  [];
-  //streak: number = 0;
-  //[{[start,end],width,left,height}]
+  timeslot = [];
 
-  constructor(){
-    window['layOutDay'] = this.layOutDay;
-  }
-
-  layOutDay(events: Calender[]): void{
-    let streak = 0;
-    let f_half = 0;
-    for(let index=0;index < events.length;index++){
-      if ((index < events.length - 1 && events[index + 1].start < events[index].end) || (index === events.length - 1 &&  events[index].start < events[index-1].end) || (f_half > 0 && events[index + 1].start > events[index].end)) {
-    
-        if (streak % 2 === 0) {
-            f_half = 1;
-            streak++;
-            this.timeslot.push({width:"280px",ev:events[index].start,height:(events[index].end-events[index].start)-8,left:"0px"})
-            //return (<TimeSlot width={'280px'} event={event} key={event.title}/>);
-        } else {
-            f_half = 0;
-            streak++;
-            this.timeslot.push({width:"280px",ev:events[index].start,height:(events[index].end-events[index].start)-8,left:"50%"})
-            //return (<TimeSlot left={'50%'} width={'280px'} event={event} key={event.title}/>);
-        }
-    } else {
-      f_half = 0;
-      this.timeslot.push({width:"100%",ev:events[index].start,height:(events[index].end-events[index].start)-8,left:"0px"})
-        //return (<TimeSlot width={'100%'} event={event} key={event.title}/>);
+  ngOnInit() {
+    window['layOutDay'] = this.layOutDay.bind(this);
+    if (localStorage.getItem('timeslot')) {
+      this.timeslot = JSON.parse(localStorage.getItem('timeslot'));
     }
   }
-  console.log(this.timeslot)
-  // this.events = events;
-  
-    // events.map((event, index) => {
-    //   console.log("dd",this);
-    //   if ((index < events.length - 1 && events[index + 1].start < event.end) || (index === events.length - 1 && streak > 0) || (streak > 0 && events[index + 1].start > event.end)) {
-    //     console.log("dd",streak);
-    //     if (streak % 2 === 0) {
-    //         streak++;
-    //         //return (<TimeSlot width={'280px'} event={event} key={event.title}/>);
-    //     } else {
-    //        streak++;
-    //         //return (<TimeSlot left={'50%'} width={'280px'} event={event} key={event.title}/>);
-    //     }
-    // } else {
-    //     //return (<TimeSlot width={'100%'} event={event} key={event.title}/>);
-    // }
-    // });
-    //this.clearCalender();
-    //validate for overlapses of time
-    //if validated push in events
 
+  layOutDay(events: Calender[]): void {
+    this.clearData();
+    this.createTimeSlot(events);
   }
-  // clearCalender(): void{
-  //   this.events = [];
-  // }
+
+  createTimeSlot(events) {
+    let streak = 0;
+    //let f_half = 0;
+    events.forEach((event, index) => {
+      if ((index < events.length - 1 && events[index + 1].start < event.end)
+        || (index === events.length - 1 && event.start < events[index - 1].end)
+        //{shoul be remove in order to fix code}|| (f_half > 0 && events[index + 1].start > event.end)
+        || (index > 0 && index < events.length - 1 && event.start < events[index - 1].end)) {
+
+        if (streak % 2 === 0) {
+         // f_half = 1;
+          streak++;
+          this.timeslot.push({ width: "45%", ev: event.start, height: (event.end - event.start), left: "0px" });
+        } else {
+         // f_half = 0;
+          streak++;
+          this.timeslot.push({ width: "45%", ev: event.start, height: (event.end - event.start), left: "50%" });
+        }
+      } else {
+       // f_half = 0;
+        this.timeslot.push({ width: "95%%", ev: event.start, height: (event.end - event.start), left: "0px" });
+      }
+    });
+    localStorage.setItem('timeslot', JSON.stringify(this.timeslot));
+    window.location.reload();
+  }
+
+  clearData() {
+    this.timeslot.length = 0;
+    localStorage.removeItem('timeslot');
+  }
 }
